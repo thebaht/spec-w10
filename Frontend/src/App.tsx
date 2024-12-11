d7828106733f23ef18e954de0d0294b735e9a6ef
spac-25 <cb@specialisterne.com>
2024-12-11 14:09:22 +0100

    Frontend header and product grid

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,17 +2,81 @@ import { createSignal, createMemo, onMount, For } from 'solid-js'
 import './App.css'
 import { createStore, unwrap } from 'solid-js/store'
 
+const BACKEND_URL = 'http://127.0.0.1:5000/'
+
+type Manufacturer = {
+  id: number
+
+  name: string;
+}
+type ProductDetails = {
+  id: number
+
+  is_hot: boolean;
+
+  weight: number;
+  cups: number;
+
+  calories: number;
+  protein: number;
+  fat: number;
+  sodium: number;
+  fiber: number;
+  carbohydrates: number;
+  sugars: number;
+  potassium: number;
+  vitamins: number;
+}
+
+type Product = {
+  [key: string]: number | string | Manufacturer | ProductDetails | undefined
+
+  id: number
+
+  name: string
+  image: string
+
+  stock: number
+  price: number
+
+  manufacturer_id: number
+  manufacturer?: Manufacturer
+
+  details_id: number
+  details?: ProductDetails
+}
+
 type Record = {
   [index: string]: null | boolean | number | string
 }
 
-function App() {
-  const [products, setProducts] = createStore<Record[]>([])
+function Header() {
+  return <div id="header">
+    <img id="logo" src="logo.png"></img>
+    <h2 id="name">Cereal</h2>
+  </div>
+}
 
-  const backend_url = 'http://127.0.0.1:5000/'
+function ProductView(props) {
+  return <div id="productView">
+    <For each={props.products}>{(product) =>
+      <ProductContainer product={product}/>
+    }</For>
+  </div>
+}
+
+function ProductContainer(props) {
+  return <div class="productContainer">
+    <img src={BACKEND_URL+props.product.image}></img>
+    <h3 id="product_title">{props.product.name}</h3>
+  </div>
+}
+
+function App() {
+  const [products, setProducts] = createStore<Product[]>([])
 
   onMount(async () => {
-    const res = await fetch(backend_url+`api/get/product`, {
+    const res = await fetch(BACKEND_URL+`api/get/product`, {
       method: "POST",
       headers: {"Content-Type": "application/json",},
       body: JSON.stringify({"mappers": ["manufacturer", "details"]})
@@ -35,19 +99,21 @@ function App() {
 
   const mutate = ({index, key}: {index: number, key: string}) => {
     let value = unwrap(products)[index][key];
-    switch (typeof(value)) {
-      case 'boolean':
-        setProducts(index, key, !value)
-        break;
-      case 'number':
-        break;
-      case 'string':
-        break;
+    if (typeof value === "number") {
+      setProducts(index, key, -value)
+    }
+    else if (typeof value === "string") {
+      if (value.charAt(0) == value.charAt(0).toLowerCase())
+        setProducts(index, key, value.toUpperCase())
+      else
+        setProducts(index, key, value.toLowerCase())
     }
   }
 
   return (
     <>
+      <Header/>
+      <ProductView products={products}/>
       <div>
         <table>
           <thead>
@@ -67,7 +133,7 @@ function App() {
         </table>
         <For each={products}>
           {(product) => {
-            return <img src={backend_url+product.image?.toString()} style="max-width: 100px; max-height: 200px; width: auto; height: auto;"></img>
+            return <img src={BACKEND_URL+product.image?.toString()} style="max-width: 100px; max-height: 200px; width: auto; height: auto;"></img>
           }}
         </For>
       </div>
