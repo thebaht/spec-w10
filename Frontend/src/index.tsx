2fdefc6036589a47eafff2201274124956621918
spac-25 <cb@specialisterne.com>
2024-12-13 14:37:48 +0100

    Indentation

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -8,44 +8,45 @@ import { Page404, MainPage, ProductPage } from './App.tsx'
 const root = document.getElementById('root')
 
 function FloatingBox(props: { onClose: () => any, children: JSX.Element }) {
-    return (
-      <div class="overlay" onClick={props.onClose}>
-        <div onClick={(e) => e.stopPropagation()}>
-          {props.children}
-          <button onClick={props.onClose}>Close</button>
-        </div>
+  return (
+    <div class="overlay" onClick={props.onClose}>
+      <div onClick={(e) => e.stopPropagation()}>
+        {props.children}
+        <button onClick={props.onClose}>Close</button>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 function Header() {
-    const [isBoxOpen, setIsBoxOpen] = createSignal(false);
+  const [isBoxOpen, setIsBoxOpen] = createSignal(false);
 
-    const openBox = () => setIsBoxOpen(true);
-    const closeBox = () => setIsBoxOpen(false);
-    return <div id="header">
-      <img id="logo" src="/logo.png"></img>
-      <h2 id="name">Cereal</h2>
+  const openBox = () => setIsBoxOpen(true);
+  const closeBox = () => setIsBoxOpen(false);
+  return <div id="header">
+    <img id="logo" src="/logo.png"></img>
+    <h2 id="name">Cereal</h2>
 
-      <button onClick={openBox}>create product</button>
-      <Show when={isBoxOpen()}>
-        <FloatingBox onClose={closeBox}>
-          <h2>Create Product</h2>
-          <input type="text" placeholder="Type something..." />
-        </FloatingBox>
-      </Show>
-    </div>
-  }
+    <button onClick={openBox}>create product</button>
+    <Show when={isBoxOpen()}>
+    <FloatingBox onClose={closeBox}>
+      <h2>Create Product</h2>
+      <input type="text" placeholder="Type something..." />
+    </FloatingBox>
+    </Show>
+  </div>
+}
 
-render(() => (
-        <>
-            <Header/>
-            <Router>
-                <Route path="/" component={MainPage} />
-                <Route path="/product/:id" component={ProductPage} matchFilters={{id: /^\d+$/}} />
-                <Route path="*" component={Page404} />
-            </Router>
-        </>
-    ),
-    root!
+render(
+  () => (
+    <>
+      <Header/>
+      <Router>
+        <Route path="/" component={MainPage} />
+        <Route path="/product/:id" component={ProductPage} matchFilters={{id: /^\d+$/}} />
+        <Route path="*" component={Page404} />
+      </Router>
+    </>
+  ),
+  root!
 )
